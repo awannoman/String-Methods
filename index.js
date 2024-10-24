3e58b1b93d509abce34edb5e1d8923dba898df50
awannoman <160035058+awannoman@users.noreply.github.com>
2024-10-24 16:10:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// let name = "noman"
+// let age = 18
+
+// console.log(`my name is ${name} my age is ${age}`);
+
+const gameName = new String('nnjshu');
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("j"));
+
+// const newString = gameName.substring(0 ,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-3, 5)
+// console.log(anotherString);
+
+// const newStringOne = "      Noman        "
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https;jjjyunomanhello"
+// console.log(url.replace("hello","new"));
+// console.log(url.includes("https"));
+
+let newGame = "nojks kjhdjsk sjkjksdb"
+console.log(newGame.split(" "));
+
+
